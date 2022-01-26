34729168e4b9532a2a1873c45a3a52db93d6393f
Bruno Valniery <brunovalniery@gmail.com>
2022-01-25 23:52:57 -0300

    First commit

---
+++ robot/app/src/main/java/robot.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author bruno
+ */
+public class robot {
+    String name = "Marciano";
+    
+    public static void main(String[] args){
+        Scanner resposta = new Scanner(System.in);
+        String resposta = "";
+        
+        do{
+            System.out.println("Olá, humano. O que você deseja? ");
+            ler.
+        }while (resposta != "tchau");
+    }
+}
